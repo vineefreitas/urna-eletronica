fd7ad45a1f508556b319da1f005ee693dcbd8cd2
vineefreitas <viniciusfreitasneri@hotmail.com>
2023-10-10 19:48:46 +0000

    feat(urnaEletronica.js): inicio das funcionalidades

--- urnaEletronica.js
+++ urnaEletronica.js
@@ -1,42 +1,63 @@
+function urnaEletronica(){
+    //aqui vai todo o código do programa....
+    console.log('Iniciando o programa');
 
+    let totalVotosCandidato1 = 0,
+        totalVotosCandidato2 = 0,
+        totalVotosCandidato3 = 0,
+        totalVotosBranco = 0,
+        totalVotosNulo = 0;
 
-// Estrutura switch-case
-const opcao =3;
+    const
 
-switch(opcao) {
-    case 1:
-        console.log('Opção é 1');
-        break;
-    case 2:
-        console.log('Opção é 2');
-        break;
-    default:
-        console.log('Nenhuma das opções');
+    do {
+        
+        // instruções
 
-        //equivalente a estrutura switch-case
-        if (opcao === 1){
-            console.log ('Opção é 1');
-        }
+    } while (condition);
 }
 
-// Estrutura de repetição: do-while (faça-enquanto)
 
-let contador = 0;
 
-do {
-    console.log ('Instrução com contador em' , contador);
-    contador += 1;
-} while (contador <= 10);
 
-// Estrutura de repetição while (enquanto)
 
-// const opcao = 0;
+// // Estrutura switch-case
+// const opcao =3;
 
-// while (opcao !== 0){
-//     console.log ('Instrução 1')
-//     console.log ('Instrução 2')
-//     console.log ('Instrução 3')
-//     console.log ('Instrução 4')
-//     console.log ('Instrução 5')
-//     console.log ('Instrução 6')
+// switch(opcao) {
+//     case 1:
+//         console.log('Opção é 1');
+//         break;
+//     case 2:
+//         console.log('Opção é 2');
+//         break;
+//     default:
+//         console.log('Nenhuma das opções');
+
+//         //equivalente a estrutura switch-case
+//         if (opcao === 1){
+//             console.log ('Opção é 1');
+//         }
 // }
+
+// // Estrutura de repetição: do-while (faça-enquanto)
+
+// let contador = 0;
+
+// do {
+//     console.log ('Instrução com contador em' , contador);
+//     contador += 1;
+// } while (contador <= 10);
+
+// // Estrutura de repetição while (enquanto)
+
+// // const opcao = 0;
+
+// // while (opcao !== 0){
+// //     console.log ('Instrução 1')
+// //     console.log ('Instrução 2')
+// //     console.log ('Instrução 3')
+// //     console.log ('Instrução 4')
+// //     console.log ('Instrução 5')
+// //     console.log ('Instrução 6')
+// // }
