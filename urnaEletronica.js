7f183a52369901e4f49650f1ddcd61109237afb3
vineefreitas <viniciusfreitasneri@hotmail.com>
2023-10-17 16:51:44 -0300

    feat(urnaEletronica.js): inclusão de alertas após a identificação do voto

--- urnaEletronica.js
+++ urnaEletronica.js
@@ -1,6 +1,6 @@
 function urnaEletronica(){
-    // aqui vai todo o código do programa....
-    console.log('Iniciando o programa');
+    // aqui vai todo o código do programa...
+    console.log('Iniciando a votação');
 
     let totalVotosCandidato1 = 0,
         totalVotosCandidato2 = 0,
@@ -23,33 +23,68 @@ function urnaEletronica(){
        console.log('| 8 | Nulo');
        console.log('| 0 | Encerrar a votação');
 
-    voto = parseInt(prompt('Digite sua opção de voto:'));
+    voto = parseInt(prompt('| 1 | Candidato 1\n| 2 | Candidato 2\n| 3 | Cantdidato 3\n| 5 | Branco\n| 8 | Nulo\n\nDigite sua opção de voto:'));
 
-switch (voto) {
-    case 1:
-        totalVotosCandidato1++;
-        console.log('O candidato 1 recebeu um voto');
-        break;
-    case 2:
-        totalVotosCandidato2++;
-        console.log('O candidato 2 recebeu um voto');
-        break;
-    case 3:
-        totalVotosCandidato3++;
-        console.log('O candidato 3 recebeu um voto');
-        break;
-    case 5:
-        totalVotosBranco++;
-        console.log('Voto em Branco');
-        break;
-    case 8:
-        totalVotosNulo++;
-        console.log('O voto foi anulado');
-        break;
-    default:
-        return;
+    switch (voto) {
+        case 1:
+            totalVotosCandidato1++;
+            console.log('O candidato 1 recebeu um voto');
+            alert('Você votou no candidato 1.');
+            break;
+        case 2:
+            totalVotosCandidato2++;
+            console.log('O candidato 2 recebeu um voto');
+            alert('Você votou no candidato 2.');
+            break;
+        case 3:
+            totalVotosCandidato3++;
+            console.log('O candidato 3 recebeu um voto');
+            alert('Você votou no candidato 3.');
+            break;
+        case 5:
+            totalVotosBranco++;
+            console.log('Voto em Branco');
+            alert('Você votou Branco.');
+            break;
+        case 8:
+            totalVotosNulo++;
+            console.log('O voto foi anulado');
+            alert('Você votou Nulo.');
+            break;
+        default:
+            return;
+    }
 }
-}
+
+// // Estrutura de repetição: do-while (faça-enquanto)
+
+// let contador = 0;
+
+// do {
+//     console.log ('Instrução com contador em' , contador);
+//     contador += 1;
+// } while (contador <= 10);
+
+// // Estrutura de repetição while (enquanto)
+
+// // const opcao = 0;
+
+// // while (opcao !== 0){
+// //     console.log ('Instrução 1')
+// //     console.log ('Instrução 2')
+// //     console.log ('Instrução 3')
+// //     console.log ('Instrução 4')
+// //     console.log ('Instrução 5')
+// //     console.log ('Instrução 6')
+// // }
+
+// https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Statements/switch
+// https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/parseInt
+// https://www.devmedia.com.br/javascript-switch/39761
+// https://cursos.alura.com.br/forum/topico-usando-o-prompt-121601
+// https://www.javascriptprogressivo.net/2018/07/Como-Receber-Dados-Usuario-Metodo-prompt.html
+// https://www.devmedia.com.br/alert-em-javascript/37208
+
 // // Estrutura switch-case equivelente a:
 
 // if (voto === 1) {
@@ -80,25 +115,3 @@ switch (voto) {
 //             console.log ('Opção é 1');
 //         }
 // }
-
-// // Estrutura de repetição: do-while (faça-enquanto)
-
-// let contador = 0;
-
-// do {
-//     console.log ('Instrução com contador em' , contador);
-//     contador += 1;
-// } while (contador <= 10);
-
-// // Estrutura de repetição while (enquanto)
-
-// // const opcao = 0;
-
-// // while (opcao !== 0){
-// //     console.log ('Instrução 1')
-// //     console.log ('Instrução 2')
-// //     console.log ('Instrução 3')
-// //     console.log ('Instrução 4')
-// //     console.log ('Instrução 5')
-// //     console.log ('Instrução 6')
-// // }
